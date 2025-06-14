549ebc3d44307417986f71ead7e9680af0558a47
00-Aryan <22f2000697@ds.study.iitm.ac.in>
2025-06-15 03:03:16 +0530

    testing push

--- Day_1.py
+++ Day_1.py
@@ -1,50 +1,84 @@
-# Basic Calculator 
-
-# num_1 = int(input("Enter first number: "))
-# num_2 = int(input("Enter second number: "))
-
-# op = input("Choose the operation you want to perform : + , - , / , * :  ")
-
-# if (op == '+'):
-#     print(num_1 + num_2)
-# elif (op == '-'):
-#     print(num_1 - num_2)
-# elif (op == '*'):
-#     print(num_1 * num_2)
-# elif (op == '/'):
-#     if num_2 == 0:
-#         print("division by zero is not possible")
-#     else:
-#         print(num_1 / num_2)
-# else:
-#     print("Invalid input")
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "pyfiglet",
+# ]
+# ///
 
 
-# Number guessing game
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     break
+#   print(x) #apple
 
-# import random 
+# fruits = ['apple', 'banana', 'cherry']
+# indexed_fruits = [(index, fruit) for index, fruit in enumerate(fruits)]
+# print(indexed_fruits) # [(0, 'apple'), (1, 'banana'), (2, 'cherry')]
 
-# x = random.randint(1,10)
-# n = None
-# while n!= x :
-#     n = int(input("Enter a number: "))
-#     if (x < n ):
-#         print("less")
-#     elif(x > n):
-#         print("more")
-#     elif(x ==n):
-#         print("Congrats you guesses it right")
+#Q1. Print a Poem with Line Numbers
+# Write a program that prints the “Twinkle Twinkle Little Star” poem, with line numbers beside each line.
+
+poem = """Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star."""
+
+# enumerate(iterable, startIndex)
+# lines = poem.split("\n")
+# for i,l in enumerate(lines, start = 1):
+#     print(i,l)
 
 
-# Write a program that prints a multi-line poem with line numbers.
-# poem = """Twinkle twinkle little star,
-# How I wonder what you are,
-# Up above the world so high,
-# Like a diamond in the sky.
-# """
-# # for i , j in enumerate(poem.strip().split('\n'),start = 1):
-# #     print(i,j)
+# Q2: Multiplication Table Generator
+# Write a Python program that:
 
-# for i , j in enumerate(poem.split('\n'), start= 1):
-#     if j.strip():
-#         print(i,j)
+# Asks the user to enter a number
+
+# Prints the multiplication table of that number from 1 to 10
+
+# 🎯 Bonus: Add validation to make sure the input is a number.
+
+
+# try :
+#     x = int(input("Enter a number to print its multiplication table: "))
+#     for i in range(1,11):
+#         print(f"{x} * {i} = {x*i}")
+# except ValueError:
+#     print("Please enter a valid number")
+
+
+# Q3: Use an External Module – ASCII Art Name
+# Question:
+# Install the external module pyfiglet
+
+# Write a program that asks for your name and prints it as ASCII art
+
+# import pyfiglet 
+# your_name = input("Enter your name")
+# print(pyfiglet.figlet_format(your_name))
+
+#     _                             _  __                                                                                                                                   
+#    / \   _ __ _   _  __ _ _ __   | |/ /   _ _ __ ___   __ _ _ __ 
+#   / _ \ | '__| | | |/ _` | '_ \  | ' / | | | '_ ` _ \ / _` | '__|
+#  / ___ \| |  | |_| | (_| | | | | | . \ |_| | | | | | | (_| | |   
+# /_/   \_\_|   \__, |\__,_|_| |_| |_|\_\__,_|_| |_| |_|\__,_|_|   
+#               |___/
+
+
---
+++ README.md
@@ -0,0 +1,10 @@
+```
+ enumerate(iterable, startIndex)  
+ <!--  -->
+try:
+    print("test")
+except ValueError:
+    print("Enter a testable number")
+<!--        -->
+pyfiglet.figlet_format(your_name)
+```
